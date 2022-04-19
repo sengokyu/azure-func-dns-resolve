3669d8ac848b8a4c0b6d0dc56b8ba04e97109a95
sengokyu <sengokyu+gh@gmail.com>
2022-04-19 14:52:40 +0900

    Complete.

--- dns-resolve-bridge/index.ts
+++ dns-resolve-bridge/index.ts
@@ -1,17 +1,32 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import { Resolver } from "dns";
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  const hostname = req.query.hostname;
+  const rrtype = req.query.rrtype;
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+  return new Promise<void>((resolv, reject) => {
+    const resolver = new Resolver();
 
+    try {
+      resolver.resolve(hostname, rrtype, (err, addresses) => {
+        const result = err ? "NG" : "OK";
+
+        context.res = {
+          headers: { "Content-Type": "application/json" },
+          body: { result, addresses },
+        };
+
+        resolv();
+      });
+    } catch (ex: any) {
+      context.log.error(ex);
+      reject();
+    }
+  });
 };
 
 export default httpTrigger;
